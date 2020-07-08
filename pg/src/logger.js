86719e9882c346d5f78bbf9e5f659f9c64cf104b
Nikita Kuzmenko <ext.nikita.kuzmenko@tieto.com>
2020-07-08 09:17:53 +0300

    Refactoring logger for PG

--- pg/src/logger.js
+++ pg/src/logger.js
@@ -1,23 +1,6 @@
 const path = require('path');
-// const { transports, createLogger, format } = require('winston');
 const winston = require('winston');
 const loggerLevel = require('./config').loggerLevel;
-
-const getFileNameAndLineNumber = () => {
-    const oldStackTrace = Error.prepareStackTrace;
-    try {
-        // eslint-disable-next-line handle-callback-err
-        Error.prepareStackTrace = (err, structuredStackTrace) => structuredStackTrace;
-        Error.captureStackTrace(this);
-        // in this example I needed to "peel" the first CallSites in order to get to the caller we're looking for
-        // in your code, the number of stacks depends on the levels of abstractions you're using
-        // in my code I'm stripping frames that come from logger module and winston (node_module)
-        const callSite = this.stack.find(line => line.getFileName().indexOf('/logger/') < 0 && line.getFileName().indexOf('/node_modules/') < 0);
-        return callSite.getFileName() + ':' + callSite.getLineNumber();
-    } finally {
-        Error.prepareStackTrace = oldStackTrace;
-    }
-};
 
 const options = {
     file: {
@@ -41,16 +24,71 @@ const logger = winston.createLogger({
     defaultMeta: {service: 'PaymentGate'},
     transports: [
         new winston.transports.Console(options.console),
-        new winston.transports.File({filename: path.join(__dirname, 'logs', 'error.log'), level: 'error', timestamp: true, ...options.file}),
+        new winston.transports.File({
+            filename: path.join(__dirname, 'logs', 'error.log'),
+            level: 'error',
+            timestamp: true, ...options.file
+        }),
         new winston.transports.File({filename: path.join(__dirname, 'logs', 'output.log'), timestamp: true}),
     ],
     exitOnError: false
 });
 
 logger.stream = {
-    write: function(message, encoding){
+    write: function (message, encoding) {
         logger.info(message);
     }
 };
 
-module.exports = logger;
+module.exports.debug = module.exports.log = function() {
+    logger.debug.apply(logger, formatLogArguments(arguments))
+};
+
+module.exports.info = function() {
+    logger.info.apply(logger, formatLogArguments(arguments))
+};
+
+module.exports.warn = function() {
+    logger.warn.apply(logger, formatLogArguments(arguments))
+};
+
+module.exports.error = function() {
+    logger.error.apply(logger, formatLogArguments(arguments))
+};
+
+module.exports.stream = logger.stream;
+
+function formatLogArguments(args) {
+    args = Array.prototype.slice.call(args);
+    const stackInfo = getStackInfo(1);
+
+    if (stackInfo) {
+        const calleeStr = '(' + stackInfo.relativePath + ':' + stackInfo.line + ')';
+        if (typeof (args[0]) === 'string') {
+            args[0] = calleeStr + ' ' + args[0]
+        } else {
+            args.unshift(calleeStr)
+        }
+    }
+    return args
+}
+
+function getStackInfo(stackIndex) {
+    const stacklist = (new Error()).stack.split('\n').slice(3)
+    const stackReg = /at\s+(.*)\s+\((.*):(\d*):(\d*)\)/gi;
+    const stackReg2 = /at\s+()(.*):(\d*):(\d*)/gi;
+
+    const s = stacklist[stackIndex] || stacklist[0];
+    const sp = stackReg.exec(s) || stackReg2.exec(s);
+
+    if (sp && sp.length === 5) {
+        return {
+            method: sp[1],
+            relativePath: path.relative(__dirname, sp[2]),
+            line: sp[3],
+            pos: sp[4],
+            file: path.basename(sp[2]),
+            stack: stacklist.join('\n')
+        }
+    }
+};
